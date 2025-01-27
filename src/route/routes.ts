a15806bde54d6f57dde1b3ab7f9d669b59c590c0
fellis-cp <60042724+fellis-cp@users.noreply.github.com>
2025-01-27 23:53:54 +0700

    Add Validator on Parameter

--- src/route/routes.ts
+++ src/route/routes.ts
@@ -1,9 +1,13 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 import { getAllpost, getBookId } from "./handler/handler";
 
 const serverRoutes = new Elysia({ prefix: "/books" })
   .get("/", getAllpost)
-  .get("/:id", ({ params: { id } }) => getBookId(id))
+  .get("/:id", ({ params: { id } }) => getBookId(id), {
+    params: t.Object({
+      id: t.Numeric(),
+    }),
+  })
   .post("/", () => "create post")
   .patch("/:id", () => "update post")
   .delete("/", () => "delete post");
