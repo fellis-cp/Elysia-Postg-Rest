e5233f62b48c19d05baa64404bb0e52f33159bec
fellis-cp <60042724+fellis-cp@users.noreply.github.com>
2025-01-27 23:35:33 +0700

    Update Params

--- src/route/routes.ts
+++ src/route/routes.ts
@@ -1,10 +1,11 @@
 import { Elysia } from "elysia";
+import { getAllpost, getBookId } from "./handler/handler";
 
-const postsRoutes = new Elysia({ prefix: "/books" })
-  .get("/", () => "get all posts")
-  .get("/:id", () => "get post by id")
+const serverRoutes = new Elysia({ prefix: "/books" })
+  .get("/", getAllpost)
+  .get("/:id", ({ params: { id } }) => getBookId(id))
   .post("/", () => "create post")
   .patch("/:id", () => "update post")
   .delete("/", () => "delete post");
 
-export { postsRoutes };
+export { serverRoutes };
